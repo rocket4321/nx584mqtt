cdca6c8e8c3abb53915f8aae9a9e7b63c6915f63
rocket4321 <77749413+rocket4321@users.noreply.github.com>
2021-03-25 13:53:53 -0700

    Add files via upload

--- setup.py
+++ setup.py
@@ -1,14 +1,25 @@
 from setuptools import setup
 
+from os import path
+
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
 setup(name='nx584mqtt',
-      version='1.1',
+      version='0.1.1.2021.3.25.rc1',
       description='NX584/NX8E Interface Library and Server',
+      long_description=long_description,
+      long_description_content_type='text/markdown',
       author='rocket4321',
       url='http://github.com/rocket4321/nx584mqtt',
       packages=['nx584mqtt'],
-      install_requires=['paho-mqtt', 'requests', 'prettytable', 'pyserial'],
+      install_requires=['paho-mqtt', 'requests', 'pyserial'],
       extras_require={
-        'full': ['flask'],
+        'full': ['flask', 'prettytable'],
+        'http': ['flask'],
+        'client': ['prettytable'],
       },
       scripts=['nx584_server', 'nx584_client'],
   )
+
